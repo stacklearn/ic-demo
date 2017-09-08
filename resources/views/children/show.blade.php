24b57ea17614f79853d8767d9106b08ae5dca2d8
Tom Boolean <tomboolean@Toms-MacBook-Pro.local>
2017-09-08 10:57:44 -0400

    make the child show view pretty

--- resources/views/children/show.blade.php
+++ resources/views/children/show.blade.php
@@ -1,3 +1,21 @@
-<h1>{{ $child->first_name }} {{ $child->last_name }}</h1>
-<h2>Born {{ $child->formatted_birth_date }} in
-  {{ $child->birth_city}}, {{ $child->birth_state}} {{ $child->birth_zip}}</h2>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">{{ $child->first_name }} {{ $child->last_name }}</div>
+
+                <div class="panel-body">
+                          Born {{ $child->formatted_birth_date }} in
+                            {{ $child->birth_city}}, {{ $child->birth_state}} {{ $child->birth_zip}}
+                </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
